6ec12210498c446afb9c58b30d76642440a9eb8d
Daniel Heyman <daniel.heyman@gmail.com>
2016-11-02 21:29:05 -0400

    remove termios

--- cs-392/src/myselect/myselect.c
+++ cs-392/src/myselect/myselect.c
@@ -3,7 +3,6 @@ Authors: Daniel Heyman & Carla Noshi
 Implements a piping process.
 */
 #include "my.h"
-#include <termios.h> 
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
@@ -61,11 +60,6 @@ void draw() {
 
 int main(int argc, char* argv[])
 {
-  struct termios initial_settings, new_settings; 
-  char key[6] = "x"; 
-  //int c = 0;
-  int num_chars;
-                
   if (argc < 2) {
       my_str("Invalid arguments");
       exit(0);
@@ -81,68 +75,38 @@ int main(int argc, char* argv[])
   }
   current = 0;
   signal(SIGWINCH, draw);
-
-  
-  // Init
   initscr();
   draw();
-
-
-  // Input
-  tcgetattr(fileno(stdin), &initial_settings); 
-  new_settings = initial_settings;
-  new_settings.c_lflag &= ~ICANON;
-  new_settings.c_lflag &= ~ECHO;
-  new_settings.c_cc[VMIN] = 1;
-  new_settings.c_cc[VTIME] = 0;
-  new_settings.c_iflag &= ~ICRNL;
   
-  if (tcsetattr(fileno(stdin), TCSAFLUSH, &new_settings) != 0) {
-    fprintf(stderr,"Could not set attributes\n"); 
-    exit(1);
-  }
-    
-  while (strcmp(key, "\033") != 0) {
-    fflush(stdout);
-    num_chars = read(fileno(stdin), key, 3);
-    key[num_chars] = '\0';
-    if (key[0] == '\015') { // enter
-      break;
-    }
-    else if(strcmp(key, "\040") == 0) { // space
+  int key = 0;
+  keypad(stdscr, TRUE);
+  noecho();
+  while(key != 27) {
+    key = getch();
+    if(key == 10) break; // enter
+    else if(key == 32) { // space
       selected[current] = !selected[current];
       if(selected[current] && ++current >= count) current = 0;
-      draw();
-    }
-    else if (strcmp(&(key[1]), "[A") == 0) { // up
+    } else if(key == KEY_UP) {
       if(--current < 0) current = count - 1;
-      draw();
-    }
-    else if (strcmp(&(key[1]), "[B") == 0) { // down
+    } else if(key == KEY_DOWN) {
       if(++current >= count) current = 0;
-      draw();
-    }
-    else if (strcmp(&(key[1]), "[D") == 0) { // left
+    } else if(key == KEY_LEFT) {
       current -= height;
       if(current < 0) current = 0;
-      draw();
-    }
-    else if (strcmp(&(key[1]), "[C") == 0) { // right
+    } else if(key == KEY_RIGHT) {
       current += height;
       if(current > count - 1) current = count - 1;
-      draw();
     }
+    draw();
   }
-    
-  tcsetattr(fileno(stdin), TCSANOW, &initial_settings);
-  endwin();
   
-  if (key[0] == '\015') { // enter
+  endwin();
+  if (key == 10) { // enter
     for(int i = 0; i < count; ++i) {
       if(selected[i]) printf("%s ", files[i]);
     }
   }
-  
   free(fileSizes);
   free(selected);
   return 0;
